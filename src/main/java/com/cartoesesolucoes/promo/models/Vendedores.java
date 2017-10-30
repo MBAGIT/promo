53ccaf9b7819d9693c9d50968d6759b0c700847b
Mohamed Babchia <mbabchia@gmail.com>
2017-10-30 22:39:26 +0100

    add Vendedores class attributs & repisitory

--- src/main/java/com/cartoesesolucoes/promo/models/Vendedores.java
+++ src/main/java/com/cartoesesolucoes/promo/models/Vendedores.java
@@ -3,6 +3,9 @@ package com.cartoesesolucoes.promo.models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.Date;
 
 @Entity
 public class Vendedores {
@@ -11,6 +14,141 @@ public class Vendedores {
     @SequenceGenerator(name = "vendedores_seq",sequenceName = "vendedores_seq")
     private Long id ;
 
+    /** nome */
+    @NotNull
+    private String nome ="adm";
+    /** pass */
+    @NotNull
+    private String pass ="adm";
+
+    /** estado */
+    @NotNull
+    private Integer estado = 1 ;
+
+    /** acesso */
+    @NotNull
+    private Integer acesso = 1 ;
+
+    /** idinframa */
+    @NotNull
+    private Integer idinframa = 1 ;
+
+    /** idempresa */
+    @NotNull
+    private Integer idempresa = 1 ;
+
+    /** smsauto */
+    @NotNull
+    private Integer smsauto = 0 ;
+
+
+    /** caminhoimagem */
+    private String caminhoimagem ;
+
+    /** uniqueid */
+    private String uniqueid ;
+
+    /** dataatualiza **/
+    @NotNull
+    @Pattern(regexp = "DD-MM-YYYY")
+    private Date dataatualiza;
+
+    /** nom_pro */
+    private String nom_pro ;
+
+
+
+    public Vendedores() {
+        // constructor for JPA
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public Integer getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Integer estado) {
+        this.estado = estado;
+    }
+
+    public Integer getAcesso() {
+        return acesso;
+    }
+
+    public void setAcesso(Integer acesso) {
+        this.acesso = acesso;
+    }
+
+    public Integer getIdinframa() {
+        return idinframa;
+    }
+
+    public void setIdinframa(Integer idinframa) {
+        this.idinframa = idinframa;
+    }
+
+    public Integer getIdempresa() {
+        return idempresa;
+    }
+
+    public void setIdempresa(Integer idempresa) {
+        this.idempresa = idempresa;
+    }
+
+    public Integer getSmsauto() {
+        return smsauto;
+    }
+
+    public void setSmsauto(Integer smsauto) {
+        this.smsauto = smsauto;
+    }
+
+    public String getCaminhoimagem() {
+        return caminhoimagem;
+    }
+
+    public void setCaminhoimagem(String caminhoimagem) {
+        this.caminhoimagem = caminhoimagem;
+    }
+
+    public String getUniqueid() {
+        return uniqueid;
+    }
+
+    public void setUniqueid(String uniqueid) {
+        this.uniqueid = uniqueid;
+    }
+
+    public Date getDataatualiza() {
+        return dataatualiza;
+    }
+
+    public void setDataatualiza(Date dataatualiza) {
+        this.dataatualiza = dataatualiza;
+    }
+
+    public String getNom_pro() {
+        return nom_pro;
+    }
+
+    public void setNom_pro(String nom_pro) {
+        this.nom_pro = nom_pro;
+    }
 
     public Long getId() {
         return id;
---
+++ src/main/java/com/cartoesesolucoes/promo/repositories/VendedoresRepository.java
@@ -0,0 +1,10 @@
+package com.cartoesesolucoes.promo.repositories;
+
+
+import com.cartoesesolucoes.promo.models.Vendedores;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface VendedoresRepository extends JpaRepository<Vendedores,Long> {
+}
+
